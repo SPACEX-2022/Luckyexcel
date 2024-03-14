e005f96f9f03cbb27250019ab955ccf5bb106e55
spacex_2020 <540240126@qq.com>
2024-03-14 16:17:09 +0800

    feat: 新增导出功能

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,19 +1,19 @@
 import {IuploadfileList} from "../src/ICommon";
 import {ILuckyJson} from "../src/ToLuckySheet/ILuck";
 
-export namespace LuckyExcel {
+export default class LuckyExcel {
     /**
      * excel file 转换为 luckysheet json 数据
      */
-    function transformExcelToLucky(excelFile:File, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
+    static transformExcelToLucky(excelFile:File, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
 
     /**
      * excel url 在线地址转换为 luckysheet json 数据
      */
-    function transformExcelToLuckyByUrl(url:string, name:string, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
+    static transformExcelToLuckyByUrl(url:string, name:string, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
 
     /**
      * luckysheet json 数据转换为 excel arraybuffer
      */
-    function transformLuckyToExcel(luckyJson: ILuckyJson, callBack?: (content: ArrayBuffer, title: string) => void): void;
+    static transformLuckyToExcel(luckyJson: ILuckyJson, callBack?: (content: ArrayBuffer, title: string) => void): void;
 }
