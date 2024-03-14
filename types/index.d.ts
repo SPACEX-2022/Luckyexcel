e060c40dfc1fc18ef985ea579f5ff50e5163bead
spacex_2020 <540240126@qq.com>
2024-03-14 16:11:11 +0800

    feat: 新增导出功能

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,19 @@
-export namespace Luckyexcel {
+import {IuploadfileList} from "../src/ICommon";
+import {ILuckyJson} from "../src/ToLuckySheet/ILuck";
+
+export namespace LuckyExcel {
     /**
      * excel file 转换为 luckysheet json 数据
      */
-    function transformExcelToLucky(): void;
+    function transformExcelToLucky(excelFile:File, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
 
     /**
      * excel url 在线地址转换为 luckysheet json 数据
      */
-    function transformExcelToLuckyByUrl(): void;
+    function transformExcelToLuckyByUrl(url:string, name:string, callBack?:(files:IuploadfileList, fs?:string)=>void): void;
 
     /**
      * luckysheet json 数据转换为 excel arraybuffer
      */
-    function transformLuckyToExcel(): void;
+    function transformLuckyToExcel(luckyJson: ILuckyJson, callBack?: (content: ArrayBuffer, title: string) => void): void;
 }
